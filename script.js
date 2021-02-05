8f70ae6d74209c16c22ccf9de6686d6e3044fa0f
serkan <s.safran84@gmail.com>
2021-02-05 14:18:30 +0100

    score table added

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 let computerChosen;
 let userChosen;
 let result; //result()
+let computerScore = document.getElementById("computer-score");
+let computerScoreCounter = 0;
+let playerScore = document.getElementById("player-score");
+let playerScoreCounter = 0;
 let displayResult = document.getElementById("gameResult");
 let computerChoice = document.getElementById("computer-choice");
 //let randomNumber = Math.round(Math.random() * (3));
 let userChoice = document.getElementById("user-choice");
 let buttons = document.querySelectorAll(".choices");
+
+console.log(playerScoreCounter);
+console.log(computerScoreCounter);
 
 //get user choice
 buttons.forEach(button => button.addEventListener("click", (event) =>{
@@ -46,43 +53,63 @@ function results(){
     return  result
   }else if(userChosen=== "rock" && (computerChosen === "scissors" || computerChosen === "lizard")) { //ok
     result = "You Win!"
-    document.getElementById("gameResult").style.color = "darkgreen";
+    document.getElementById("gameResult").style.color = "green";
+    playerScoreCounter++
+    playerScore.textContent = playerScoreCounter.toString()
     return result
   }else if(computerChosen === "rock" && (userChosen === "scissors" || userChosen === "lizard")) {
     result = "You Lost!"
     document.getElementById("gameResult").style.color = "red";
+    computerScoreCounter++
+    computerScore.textContent = computerScoreCounter.toString()
     return result
   }else if (userChosen === "scissors" && (computerChosen === "paper"|| computerChosen === "lizard")) {
     result = "You Win!"
-    document.getElementById("gameResult").style.color = "darkgreen";
+    document.getElementById("gameResult").style.color = "green";
+    playerScoreCounter++
+    playerScore.textContent = playerScoreCounter.toString()
     return result
   }else if(computerChosen === "scissors" && (userChosen === "paper" || userChosen === "lizard")){
     result = "You Lost!"
     document.getElementById("gameResult").style.color = "red";
+    computerScoreCounter++
+    computerScore.textContent = computerScoreCounter.toString()
     return result
   }else if (userChosen === "spock" && (computerChosen === "rock"|| computerChosen === "scissors")) {
     result = "You Win!"
-    document.getElementById("gameResult").style.color = "darkgreen";
+    document.getElementById("gameResult").style.color = "green";
+    playerScoreCounter++
+    playerScore.textContent = playerScoreCounter.toString()
     return result
   }else if(computerChosen === "spock" && (userChosen === "rock" || userChosen === "scissors")) {
     result = "You Lost!"
     document.getElementById("gameResult").style.color = "red";
+    computerScoreCounter++
+    computerScore.textContent = computerScoreCounter.toString()
     return result
   }else if (userChosen === "lizard" && (computerChosen === "paper" || computerChosen === "spock")) {
     result = "You Win!"
-    document.getElementById("gameResult").style.color = "darkgreen";
+    document.getElementById("gameResult").style.color = "green";
+    playerScoreCounter++
+    playerScore.textContent = playerScoreCounter.toString()
     return result
   }else if(computerChosen === "lizard" && (userChosen === "paper" || userChosen === "spock")) {
     result = "You Lost!"
     document.getElementById("gameResult").style.color = "red";
+    computerScoreCounter++
+    computerScore.textContent = computerScoreCounter.toString()
     return result
   }else if (userChosen === "paper" && (computerChosen === "spock" || computerChosen === "rock")) {
     result = "You Win!"
-    document.getElementById("gameResult").style.color = "darkgreen";
+    document.getElementById("gameResult").style.color = "green";
+    playerScoreCounter++
+    playerScore.textContent = playerScoreCounter.toString()
     return result
   }else if(computerChosen === "paper" && (userChosen === "spock" || userChosen === "rock")) {
     result = "You Lost!"
     document.getElementById("gameResult").style.color = "red";
+    computerScoreCounter++
+    computerScore.textContent = computerScoreCounter.toString()
     return result
     }
     // displayResult.innerHTML = result
